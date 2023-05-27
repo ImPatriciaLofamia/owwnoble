7d54efd763c68916f0aaca4bc390cbf23e733021
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-05-27 18:44:27 +0800

    add page about me

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const about = () => {
+  return (
+    <Layout>
+      <div>About me page</div>
+    </Layout>
+  );
+};
+
+export default about;
