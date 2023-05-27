31fde94f707648159cce39e9cd91c225d501e1cc
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-05-27 18:44:39 +0800

    add home page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="">
+        <h1>Index</h1>
+      </div>
+    </Layout>
+  );
+}
