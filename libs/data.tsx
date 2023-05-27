63d63401fe8c9c570fa2b1214dcb39d643005bec
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-05-27 22:27:50 +0800

    fix data and add other skills

---
+++ libs/data.tsx
@@ -0,0 +1,47 @@
+import {
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNextdotjs,
+  SiGithub,
+  SiFigma
+} from "react-icons/si";
+
+export const skills = [
+  {
+    id: 1,
+    title: `JavaScript`,
+    icon: <SiJavascript size={50} />,
+    contents: ``,
+  },
+  {
+    id: 2,
+    title: `TypeScript`,
+    icon: <SiTypescript size={50} />,
+    contents: ``,
+  },
+  {
+    id: 3,
+    title: `React/Native`,
+    icon: <SiReact size={50} />,
+    contents: ``,
+  },
+  {
+    id: 4,
+    title: `Next JS`,
+    icon: <SiNextdotjs size={50} />,
+    contents: ``,
+  },
+  {
+    id: 5,
+    title: `Version Control`,
+    icon: <SiGithub size={50} />,
+    contents: ``
+  },
+  {
+    id: 6,
+    title: `Figma Design`,
+    icon: <SiFigma size={50} />,
+    contents: ``
+  }
+];
